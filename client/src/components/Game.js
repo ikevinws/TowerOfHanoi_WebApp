747582cd2e3c1ee314d50b3e63f8acc52c66a380
ikevinws <ikevinwang@ufl.edu>
2020-08-23 01:55:49 -0400

    loading screen for game

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Spinner } from 'react-bootstrap';
 
 const Game = () => {
+    const [unityInstanceLoaded, setUnityInstanceLoaded] = useState(false);
     useEffect(() => {
         const canvasElement = document.querySelector('#unity-canvas');
         window
@@ -14,17 +15,33 @@ const Game = () => {
                 productName: 'Tower Of Hanoi',
                 productVersion: '0.1',
             })
-            .then((unityInstance) => {})
+            .then((unityInstance) => {
+                if (unityInstance) {
+                    setUnityInstanceLoaded((prevLoaded) => !prevLoaded);
+                }
+            })
             .catch(() => {
                 // alert(message);
                 window.location.reload();
             });
     }, []);
 
+    /*
+    cant use tenary to hide/show canvas because createUnityInstance needs
+     canvas element with id=unity-canvas to exist when component is mounted
+    */
+    const displayGame = unityInstanceLoaded ? '' : 'd-none';
+    const displayLoading = unityInstanceLoaded ? 'd-none' : 'game-canvas loading-game-canvas';
     return (
         <>
             <Container className="text-center p-0">
-                <canvas id="unity-canvas" className="game-canvas " />
+                <canvas id="unity-canvas" className={`${displayGame} game-canvas`} />
+                <div className={displayLoading}>
+                    <Spinner animation="border" role="status" variant="light">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                    <span className="ml-2">Loading Game...</span>
+                </div>
             </Container>
         </>
     );
