43f4608647005ccc34e9f8995faacf9147f05ad7
ikevinws <ikevinwang@ufl.edu>
2020-08-30 14:37:23 -0400

    signup post request

--- client/src/components/signUpForm.js
+++ client/src/components/signUpForm.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Image, Button } from 'react-bootstrap';
+import axios from 'axios';
+import FormAlert from './formAlert';
 
 const SignUpForm = () => {
+    // form data handling
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password: '',
+        confirmedPassword: ''
+    });
+    // error handling
+    const [showSignInError, setShowSignInError] = useState(false);
+    const [errorArr, setErrorArr] = useState([]);
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setSignUpData({ ...signUpData, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const res = await axios.post(
+                '/api/user/signup',
+                {
+                    username: signUpData.username,
+                    password: signUpData.password,
+                    confirmedPassword: signUpData.confirmedPassword
+                },
+                { withCredentials: true }
+            );
+            if (res.status === 201) {
+                window.location.href = '/signin';
+            }
+        } catch (err) {
+            if (err.response) {
+                // client received an error response (5xx, 4xx)
+                if (err.response.data.errors) {
+                    setErrorArr([...err.response.data.errors]);
+                }
+            } else {
+                // anything else
+                setErrorArr((prevMsgs) => [
+                    ...prevMsgs,
+                    'An error occurred on the server. Please try again'
+                ]);
+            }
+            setShowSignInError(true);
+        }
+    };
+    const errMsgsComponent = (errorArr) => {
+        const errMsgsComponent = errorArr.map((err) => <li>{err.msg}</li>);
+        console.log(errMsgsComponent);
+        return <ul className="m-0 pl-4">{errMsgsComponent}</ul>;
+    };
+
     return (
-        <Form className="signup-form">
+        <Form className="signup-form" onSubmit={handleSubmit}>
+            <FormAlert
+                showAlert={showSignInError}
+                setShowAlert={setShowSignInError}
+                message={errMsgsComponent(errorArr)}
+                heading={'Invalid Sign Up'}
+            />
             <Image
                 src="/images/CreateAccount_Icon.svg"
                 alt="Create Account Icon"
@@ -15,11 +74,25 @@ const SignUpForm = () => {
             <h3 className="h3 my-3 font-weight-normal ">Signup</h3>
             <Form.Group controlId="Username" className="mb-0">
                 <Form.Label srOnly>Username</Form.Label>
-                <Form.Control type="text" placeholder="Username" className="rounded-top" />
+                <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    className="rounded-top"
+                    name="username"
+                    value={signUpData.username}
+                    onChange={handleChange}
+                />
             </Form.Group>
             <Form.Group controlId="Password" className="mb-0">
                 <Form.Label srOnly>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" className="rounded-0" />
+                <Form.Control
+                    type="password"
+                    placeholder="Password (6 characters)"
+                    className="rounded-0"
+                    name="password"
+                    value={signUpData.password}
+                    onChange={handleChange}
+                />
             </Form.Group>
             <Form.Group controlId="ConfirmedPassword">
                 <Form.Label srOnly>Confirm Password</Form.Label>
@@ -27,6 +100,9 @@ const SignUpForm = () => {
                     type="password"
                     placeholder="Confirm Password"
                     className="rounded-bottom"
+                    name="confirmedPassword"
+                    value={signUpData.confirmedPassword}
+                    onChange={handleChange}
                 />
             </Form.Group>
             <Button varitant="primary" type="submit" block size="lg">
