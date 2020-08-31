59b8971d7d8df4173831386ceb276058553d5560
ikevinws <ikevinwang@ufl.edu>
2020-08-30 21:54:20 -0400

    hide certain narbar links depending on user auth

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Navbar, Nav, Form } from 'react-bootstrap';
 import { useBackground, useBackgroundUpdate } from '../utils/backgroundContext';
+import { useAuthContext } from '../utils/authContext';
 import axios from 'axios';
 
 const AppNavbar = () => {
     const darkMode = useBackground();
     const toggleDarkMode = useBackgroundUpdate();
+    const auth = useAuthContext();
     const handleSignOut = async () => {
         try {
-            const res = await axios.get('/api/user/signout');
-            if (res.status === 200) {
-                //redirect
+            if (auth.isAuth) {
+                const res = await axios.get('/api/user/signout');
+                if (res.status === 200) {
+                    auth.setIsAuth(false);
+                }
             }
         } catch {
             alert('An error has occured. Please try again.');
@@ -35,9 +39,14 @@ const AppNavbar = () => {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="m-2">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/signin">Sign In</Nav.Link>
-                    <Nav.Link href="/signup">Create Account</Nav.Link>
-                    <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
+                    <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
+                    {auth.isAuth ? null : (
+                        <>
+                            <Nav.Link href="/signin">Sign In</Nav.Link>
+                            <Nav.Link href="/signup">Create Account</Nav.Link>
+                        </>
+                    )}
+                    {auth.isAuth ? <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link> : null}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
